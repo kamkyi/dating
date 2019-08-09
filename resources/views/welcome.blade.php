b424372c529bf6dc64385479e55d79c6328c60aa
Thiha <waihynhtun1994@gmail.com>
2019-08-09 22:00:58 +0630

    update welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,6 +70,7 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
+                        <a href="{{url('/redirect')}}" >Login with Facebook</a>
                         <a href="{{ route('login') }}">Login</a>
 
                         @if (Route::has('register'))
