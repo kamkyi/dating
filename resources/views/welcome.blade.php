4ce0c2ccf3011e41b12e7e75358dfbf716f3eb15
Thiha <waihynhtun1994@gmail.com>
2019-08-10 16:59:59 +0630

    update buttons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,19 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Dating</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
+        <!-- bootstrap cdn -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+        <!-- bootstrap cdn end -->
         <style>
             html, body {
                 background-color: #fff;
@@ -99,6 +103,13 @@
                          :D
                      </p>
                      <p>Born Date : 9 August , 2019 FRI , 11:00 pm </p>
+                </center>
+                <center>
+                         <div class="btn-group">
+                                <button type="button" class="btn">Find NearBy Mate</button>
+                                <button type="button" class="btn">Fun Place</button>
+                                <button type="button" class="btn">Love Astroger</button>
+                         </div>
                 </center>
             </div>
         </div>
